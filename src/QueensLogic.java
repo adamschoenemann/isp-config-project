6d2a49f505a71bfaa9688d6e4ec7914bcf5699ec
Adam Schønemann <adamschoenemann@gmail.com>
2016-03-31 15:18:30 +0200

    implement row constraints! probably incorrectly!

--- src/QueensLogic.java
+++ src/QueensLogic.java
@@ -30,6 +30,35 @@ public class QueensLogic {
         return board;
     }
 
+    private BDD initBDD(int size) {
+
+        BDDFactory fact = JFactory.init(2000000, 200000);
+        fact.setVarNum(size * size);
+
+
+        BDD bdd = fact.one();
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                bdd = rowConstraint(fact, bdd, i, j, size);
+            }
+        }
+
+        return bdd;
+
+    }
+
+    private BDD rowConstraint(BDDFactory fact, BDD bdd, int i, int j, int size) {
+        BDD antecedent = fact.ithVar(i * size + j);
+        BDD consequent = fact.one();
+
+        for (int l = 0; l < size; l++) {
+            if (l == j) continue;
+            consequent = consequent.and(fact.nithVar(i * size + l));
+        }
+        BDD expr = antecedent.imp(consequent);
+        return bdd.and(expr);
+    }
+
     public boolean insertQueen(int column, int row) {
 
         if (board[column][row] == -1 || board[column][row] == 1) {
